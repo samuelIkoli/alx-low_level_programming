e9e8c09dd99a2279f6cb252839141da6647045f9
samuelikoli <ayibanimiikoli@gmail.com>
2022-04-08 14:06:33 +0100

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point prints all lower case alphabets
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	printf("%d\n", 75/10);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point prints all lower case alphabets
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
